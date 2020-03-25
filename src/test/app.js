354c79c56e55113c354caa2a3e986c26f35ec6ef
Khurshid Bhuyan <khurs10101@gmail.com>
2020-03-25 18:39:28 +0530

    implemented image crop

--- src/test/app.js
+++ src/test/app.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import ReactCrop from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css' 
 import { saveAs } from 'file-saver'
-const fs= require('web-fs')
+// const fs= require('web-fs')
+const axios= require('axios')
 
 class App extends PureComponent {
     state = {
       src: null,
       crop: {
         unit: '%',
-        width: 30
       },
     };
   
@@ -41,13 +41,17 @@ class App extends PureComponent {
   
     async makeClientCrop(crop) {
       if (this.imageRef && crop.width && crop.height) {
-        const croppedImageUrl = await this.getCroppedImg(
+        const holder = await this.getCroppedImg(
           this.imageRef,
           crop,
           'newFile.jpeg'
         );
-        console.log(croppedImageUrl);
+        let croppedImageUrl= holder[0]
+        let blob= holder[1]
+        //console.log(holder[0]);
         this.setState({ croppedImageUrl });
+        this.setState({blob})
+        //console.log(holder[0])
       }
     }
   
@@ -72,6 +76,8 @@ class App extends PureComponent {
       );
   
       return new Promise((resolve, reject) => {
+        // this.setState({imageFile: canvas.toDataURL('image/jpeg',0.5)})
+        // console.log(canvas.toDataURL('image/jpeg'))
         canvas.toBlob(blob => {
           if (!blob) {
             //reject(new Error('Canvas is empty'));
@@ -83,14 +89,27 @@ class App extends PureComponent {
           
           window.URL.revokeObjectURL(this.fileUrl);
           this.fileUrl = window.URL.createObjectURL(blob);
-          //console.log(this.fileUrl);
-          resolve(this.fileUrl);
-        }, 'image/jpeg');
+          let holder =[this.fileUrl, blob]
+          console.log(holder);
+          resolve(holder);
+        }, 'image/jpeg', 1.0);
       });
+    }
+
+    uploadImage= ()=>{
+      let imageFile= this.state.blob
+      let data= new FormData();
+      data.append('file',imageFile, "temp.jpeg")
+      axios.post("http://localhost:5000/upload",data,{})
+            .then((response)=>{
+              console.log(response)
+              this.setState({convertText: response.data.output})
+              console.log(this.state.convertText)
+            })
     }
   
     render() {
-      const { crop, croppedImageUrl, src } = this.state;
+      const { crop, croppedImageUrl, src, convertText } = this.state;
   
       return (
         <div className="App">
@@ -101,7 +120,6 @@ class App extends PureComponent {
             <ReactCrop
               src={src}
               crop={crop}
-              ruleOfThirds
               onImageLoaded={this.onImageLoaded}
               onComplete={this.onCropComplete}
               onChange={this.onCropChange}
@@ -110,9 +128,16 @@ class App extends PureComponent {
           {croppedImageUrl && (
             <div>
                 <img id="cropped_image" alt="Crop" style={{ maxWidth: '100%' }} src={croppedImageUrl} />
-                <button>Send to OCR</button>
+                <button onClick={this.uploadImage}>Send to OCR</button>
             </div>
           )}
+          {
+            convertText && (
+              <div>
+                <h2>{convertText}</h2>
+              </div>
+            )
+          }
         </div>
       );
     }
